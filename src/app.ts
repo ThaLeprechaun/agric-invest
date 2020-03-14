99ee0e3f411676204ea1da7e3d597785673ae929
thaleprechaun <oakinbiyiusman@gmail.com>
2020-03-14 22:47:21 +0100

    feature(setup): setup server

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,37 @@
 import createError from 'http-errors';
 import express from 'express';
 import cors from 'cors';
-import compression from 'compression';
 import morgan from 'morgan';
 import graphQLHTTP from 'express-graphql';
 import path from 'path';
-import fs from 'fs';
 
-import apiRouter from './routes/index';
+import indexRouter from './routes/index';
+import authRouter from './routes/auth';
+import userRouter from './routes/user';
+import farmRouter from './routes/farms';
+import investRouter from './routes/investments';
 import schema from './schema';
 
 const app = express();
 
-// Setup Request logging
-const logFormat = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+// view engine setup
+// app.engine('pug', require('pug').__express);
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
 
-app.use(
-  morgan(logFormat, {
-    skip: function(_req, res) {
-      if (process.env.NODE_ENV === 'test') {
-        return true;
-      }
-
-      return res.statusCode < 400;
-    },
-    stream: process.stderr,
-  }),
-);
-
-app.use(
-  morgan(logFormat, {
-    skip: function(_req, res) {
-      if (process.env.NODE_ENV === 'test') {
-        return true;
-      }
-
-      return res.statusCode >= 400;
-    },
-    stream: process.stdout,
-  }),
-);
-
-app.disable('x-powered-by');
-app.use(compression());
-app.use(cors());
+//Middlewares
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
 
-app.use('/api', apiRouter);
+//Route middlewares
+app.use('/', indexRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/farms', farmRouter);
+app.use('/api/v1/investments', investRouter);
 
 app.use(
   '/graphql',
@@ -57,32 +41,24 @@ app.use(
   }),
 );
 
-const clientPath = path.join(__dirname, '../', 'client/build');
-
-if (fs.existsSync(clientPath)) {
-  app.use(express.static(clientPath));
-  app.get('/*', (_req, res) => {
-    res.sendFile(path.join(clientPath, 'index.html'));
-  });
-}
-
 // catch 404 and forward to error handler
-app.use(function(
-  _req: express.Request,
-  _res: express.Response,
-  next: express.NextFunction,
-) {
+app.use(function(_req, _res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err: any, req: express.Request, res: express.Response) {
+app.use(function(
+  err: Error,
+  req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction,
+) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status((<any>err).status || 500);
   res.render('error');
 });
 
