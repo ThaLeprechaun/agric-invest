af2b9c08dd0c604dac649e7d4fde90d62ab3e45b
thaleprechaun <oakinbiyiusman@gmail.com>
2020-03-24 08:07:44 +0100

    Modify file

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,32 @@
 /// <reference types="react-scripts" />
+
+import { AxiosRequestConfig } from 'axios';
+import { Action } from 'redux';
+
+export interface RegisterType {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  password: string;
+  userCategory: string;
+}
+
+export interface LoginType {
+  email: string;
+  password: string;
+}
+
+export interface ConfigType extends AxiosRequestConfig {
+  header: {
+    'Content-Type': string;
+  };
+}
+
+export interface ActionType extends Action {
+  payload?: {
+    token?: string | any;
+    user?: object;
+    error: string;
+  };
+}
